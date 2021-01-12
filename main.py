b98b8cbaf058c0a8810a4f93d00bab01bc86f76f
FusedAndrew <72750640+FusedAndrew@users.noreply.github.com>
2021-01-12 10:31:09 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+while True:
+    if input.sound_level() >= 20:
+        light.rainbow_animation
+        music.magic_wand.loop()
---
+++ main.ts
@@ -0,0 +1,7 @@
+while (true) {
+    if (input.soundLevel() >= 20) {
+        light.rainbowAnimation
+        music.magicWand.loop()
+    }
+    
+}
